3f5b1a8b61036863cf7d3b5b3cc99cdf9c09e5a1
isabelleli <ili@wellesley.edu>
2019-08-19 20:36:33 -0400

    Updates

---
+++ precache-manifest.4a75589b9ee2d16064bb83961c2383df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59ffaef5e89ebfc9e03c928355e0dd0",
+    "url": "/coursepickerv2/index.html"
+  },
+  {
+    "revision": "50616f155e325e2cc24b",
+    "url": "/coursepickerv2/static/css/main.51593e8c.chunk.css"
+  },
+  {
+    "revision": "d5ab0d85dee1c39b81ba",
+    "url": "/coursepickerv2/static/js/2.2c044b4e.chunk.js"
+  },
+  {
+    "revision": "50616f155e325e2cc24b",
+    "url": "/coursepickerv2/static/js/main.fa31cc55.chunk.js"
+  },
+  {
+    "revision": "6895d8a276e7eab68215",
+    "url": "/coursepickerv2/static/js/runtime~main.0f5aedcc.js"
+  },
+  {
+    "revision": "4ea8bd09c8258512e96afe1a25a34958",
+    "url": "/coursepickerv2/static/media/wellesley.4ea8bd09.png"
+  }
+]);
